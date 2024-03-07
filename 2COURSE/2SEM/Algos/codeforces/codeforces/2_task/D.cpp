76dd3107f6172589ca37287bc997606b6460c0a5
graevsky <graevsky@bk.ru>
2024-03-07 15:23:10 +0300

    algos task d

--- 2COURSE/2SEM/Algos/codeforces/codeforces/2_task/D.cpp
+++ 2COURSE/2SEM/Algos/codeforces/codeforces/2_task/D.cpp
@@ -7,56 +7,49 @@
 using namespace std;
 
 
-void get_mn(int a, map<int,int>& mn){
-    for(int i = 2; i <= sqrt(a);i++){
-        while( a % i == 0){
+void get_mn(int a, map<int, int> &mn) {
+    for (int i = 2; i <= sqrt(a); i++) {
+        while (a % i == 0) {
             mn[i]++;
             a /= i;
         }
     }
-    if(a != 1){
+    if (a != 1) {
         mn[a]++;
     }
 }
-
-
 
 int main() {
     int t;
     cin >> t;
 
 
-    for(int i = 0;i < t; i++){
+    for (int i = 0; i < t; i++) {
         int n;
         cin >> n;
         vector<int> inp;
-        map<int,int> mn;
-        for(int j =0;j < n; j++){
-
+        map<int, int> mn;
+        for (int j = 0; j < n; j++) {
             int a;
             cin >> a;
-           inp.push_back(a);
-           get_mn(a,mn);
-
+            inp.push_back(a);
+            get_mn(a, mn);
         }
 
         bool flag = true;
 
-        for(auto& iter : mn){
-            if(iter.second % n != 0){
+        for (auto &iter: mn) {
+            if (iter.second % n != 0) {
                 flag = false;
                 break;
             }
         }
 
-        if(flag){
+        if (flag) {
             cout << "YES" << endl;
-        }else{
+        } else {
             cout << "NO" << endl;
         }
-
     }
-
-
     return 0;
 }
--- 2COURSE/2SEM/Algos/codeforces/test.cpp
+++ 2COURSE/2SEM/Algos/codeforces/test.cpp
@@ -1,27 +1,11 @@
-#include <iostream>
-#include <string>
-
-std::string ropeOperation(std::string& s, int i, int j, int k) {
-    std::string cut = s.substr(i, j - i + 1); // Вырезаем подстроку
-    s.erase(i, j - i + 1); // Удаляем вырезанную часть из исходной строки
-    if (k == 0) {
-        s = cut + s; // Вставляем вырезанное в начало, если k == 0
-    } else {
-        s.insert(k, cut); // Вставляем вырезанное после k-го символа
+/*
+    int div = 2;
+    while(div*div <= a){
+        while(a % div == 0){
+            mn[div]++;
+            a /= div;
+        }
+        div++;
     }
-    return s;
-}
-
-int mainasd() {
-    std::string s;
-    std::cin >> s;
-    int q;
-    std::cin >> q;
-    while (q--) {
-        int i, j, k;
-        std::cin >> i >> j >> k;
-        ropeOperation(s, i, j, k == 0 ? 0 : k);
-    }
-    std::cout << s << std::endl;
-    return 0;
-}
+    if(a > 1) mn[a]++;
+     */
