395b0adbc64e6a6709bb06051b83f1f29b9ccbbb
graevsky <graevsky@bk.ru>
2024-03-27 12:32:53 +0300

    wtf

--- 2COURSE/2SEM/Optimization/lab3/main.py
+++ 2COURSE/2SEM/Optimization/lab3/main.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 
 
@@ -9,7 +8,7 @@ def f(x):
 def solution(x1, del_x, e):
     i = 1
     while True:
-        i+=1
+        i += 1
         x2 = x1 + del_x
         f1 = f(x1)
         f2 = f(x2)
@@ -28,9 +27,9 @@ def solution(x1, del_x, e):
             x_min = x2
         else:
             x_min = x3
-        #print("X1 ",x1, " X2 ", x2, " X3 ", x3)
-        #print("Y1 ", f1, " Y2 ", f2, " Y3 ", f3)
-        num = (x2**2 - x3**2) * f1 + (x3**2 - x1**2) * f2 + (x1**2 - x2**2) * f3
+        # print("X1 ",x1, " X2 ", x2, " X3 ", x3)
+        # print("Y1 ", f1, " Y2 ", f2, " Y3 ", f3)
+        num = (x2 ** 2 - x3 ** 2) * f1 + (x3 ** 2 - x1 ** 2) * f2 + (x1 ** 2 - x2 ** 2) * f3
         den = (x2 - x3) * f1 + (x3 - x1) * f2 + (x1 - x2) * f3
 
         if den == 0:
@@ -38,11 +37,11 @@ def solution(x1, del_x, e):
             continue
 
         x_over = 0.5 * num / den
-        #print("X over ", x_over)
+        # print("X over ", x_over)
         f_over = f(x_over)
 
         if abs((f_min - f_over) / f_over) < e and abs((x_min - x_over) / x_over) < e:
-            #print("Iter amount: ",i)
+            # print("Iter amount: ",i)
             return round(x_over, 5)
         if x_over not in [min(x1, x3), max(x1, x3)]:
             x1 = x_over
@@ -61,19 +60,19 @@ def solution(x1, del_x, e):
 
 # f(x) = (1 / 3) * pow(x, 3) - 5 * x + x * np.log(x)
 
-#inp_x = 1.5
-#inp_dx = 0.001
-#inp_e = 0.00001
+inp_x = 1.5
+inp_dx = 1
+inp_e = 0.00001
 
-print(
-    "For f(x) = (1 / 3) * pow(x, 3) - 5 * x + x * np.log(x) enter starting x, delta x, epsilon:"
-)
+# print(
+#    "For f(x) = (1 / 3) * pow(x, 3) - 5 * x + x * np.log(x) enter starting x, delta x, epsilon:"
+# )
 
-inp_x = input()
-inp_dx = input()
-inp_e = input()
+# inp_x = input()
+# inp_dx = input()
+# inp_e = input()
 
 
-res_x = solution(inp_x,inp_dx,inp_e)
+res_x = solution(inp_x, inp_dx, inp_e)
 
 print("Min x: ", res_x, " and min f(x): ", round(f(res_x), 5))
