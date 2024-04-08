de209e06c0133e8be8242c3dc83edc84d427ed9d
graevsky <graevsky@bk.ru>
2024-04-08 17:34:16 +0300

    OPI lab3 final release

--- 2COURSE/2SEM/OPI/lab3/src/test/java/org/example/UtilsTest.java
+++ 2COURSE/2SEM/OPI/lab3/src/test/java/org/example/UtilsTest.java
@@ -13,11 +13,68 @@ public class UtilsTest {
     }
 
     @Test
-    public void testCheckHitFalse() throws InterruptedException {
+    public void testCheckHitFalse() {
         Utils util = new Utils(4, 4, 3);
-        Thread.sleep(5000);
         Assert.assertFalse(util.checkHit());
     }
 
+    @Test
+    public void testRectangleAreaHit() {
+        Utils util = new Utils(-1, 1, 2);
+        Assert.assertTrue(util.checkHit());
+    }
 
+    @Test
+    public void testRectangleAreaMiss() {
+        Utils util = new Utils(-3, 1.5, 2);
+        Assert.assertFalse(util.checkHit());
+    }
+
+    @Test
+    public void testCircleAreaHit() {
+        Utils util = new Utils(0.5, 0.5, 2);
+        Assert.assertTrue(util.checkHit());
+    }
+
+    @Test
+    public void testCircleAreaMiss() {
+        Utils util = new Utils(1, 1, 1);
+        Assert.assertFalse(util.checkHit());
+    }
+
+    @Test
+    public void testTriangleAreaHit() {
+        Utils util = new Utils(-1, -1, 2);
+        Assert.assertTrue(util.checkHit());
+    }
+
+    @Test
+    public void testTriangleAreaMiss() {
+        Utils util = new Utils(-2, -3, 2);
+        Assert.assertFalse(util.checkHit());
+    }
+
+    @Test
+    public void testValidationTrue() {
+        Utils util = new Utils(0, 0, 3);
+        Assert.assertTrue(util.validate());
+    }
+
+    @Test
+    public void testValidationFalseX() {
+        Utils util = new Utils(5, 2, 3);
+        Assert.assertFalse(util.validate());
+    }
+
+    @Test
+    public void testValidationFalseY() {
+        Utils util = new Utils(2, -6, 3);
+        Assert.assertFalse(util.validate());
+    }
+
+    @Test
+    public void testValidationFalseR() {
+        Utils util = new Utils(2, 2, 0);
+        Assert.assertFalse(util.validate());
+    }
 }
