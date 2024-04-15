32e8d5f2530cec2482a6929f4f18b11590f1dc0f
red <red@mail.ru>
2024-04-15 16:21:17 +0300

    CSA lab3 isa, scheme, code

---
+++ 2COURSE/2SEM/Csa/lab3/code/isa.py
@@ -0,0 +1,48 @@
+import json
+from collections import  namedtuple
+from enum import Enum
+
+class Opcode(str, Enum):
+    "Операции"
+    CR = "cr"
+    LOAD_ADDR = "load_addr"
+    ACCEPT = "accept"
+    SWAP = "swap"
+    TYPE = "type"
+    DUP = "dup"
+    PRINT_STRING = "print_string"
+
+    def __str__(self):
+        return str(self.value)
+
+
+"""Связб с исходным кодом"""
+class Term(namedtuple("Term", "line pos symbol")):
+    pass
+
+# машкод в файл
+def write_code(filename, code):
+    with open(filename,"w",encoding="utf-8") as file:
+        """Из примера
+        buf = []
+        for instr in code:
+            buf.append(json.dumps(instr))
+        file.write("[" + ",\n ".join(buf) + "]")
+        """
+        json.dump(code,file,indent=4)
+
+
+# чтение машкода из файла
+def read_code(filename):
+    with open(filename,"r",encoding="utf-8") as file:
+        code = json.load(file)
+
+
+    # Нужно?
+    for instruction in code:
+        instruction['opcode'] = Opcode(instruction['opcode'])
+        if 'term' in instruction:
+            instruction['term'] = Term(*instruction['term'])
+
+
+    return code
